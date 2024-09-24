0eb809522dd6c1283726a9ec4898862e6fbf09d5
Dedealtograu <matias.jms14@gmail.com>
2024-09-23 22:48:28 -0300

    Final aula 6

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, nativeTheme } = require('electron')
+const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron')
 
 const createWindow = () => {
     //nativeTheme.themeSource = 'light'
@@ -13,11 +13,31 @@ const createWindow = () => {
             nodeIntegration: true
         }
     })
+
+    // Menu personalizado
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
     win.loadFile('./src/views/index.html')
+}
+
+// Janela sobre
+const aboutWindow = () => {
+    const about = new BrowserWindow({
+        width: 360,
+        height: 220,
+        icon: './src/public/img/pc.png',
+        autoHideMenuBar: true,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    about.loadFile('./src/views/sobre.html')
 }
 
 app.whenReady().then(() => {
     createWindow()
+    //aboutWindow()
     
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
@@ -31,3 +51,65 @@ app.on('window-all-closed', () => {
         app.quit()
     }
 })
+
+// Template do menu
+const template = [
+    {
+        label: 'Arquivo',
+        submenu: [
+            {
+                label: 'Sair',
+                click: () => app.quit(),
+                accelerator: 'Alt+F4'
+            }
+        ]
+    },
+    {
+        label: 'Exibir',
+        submenu: [
+            {
+                label: 'Recarregar',
+                role: 'reload'
+            },
+            {
+                label: 'Ferramentas do desenvolvedor',
+                role: 'toggleDevTools'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Aplicar zoom',
+                role: 'zoomIn'
+            },
+            {
+                label: 'Diminuir zoom',
+                role: 'zoomOut'
+            },
+            {
+                label: 'Zoom normal',
+                role: 'resetZoom'
+            }
+        ]
+    },
+    {
+        label: 'Ajuda',
+        submenu: [
+            {
+                label: 'docs',
+                click: () => {
+                    shell.openExternal('https://github.com/Dedealtograu/electron')
+                }
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Sobre',
+                click: () => {
+                    aboutWindow()
+                }
+            }
+        ]
+    }
+]
